3fe65c372c5a3f05b5b42b51fdec7497f0d2432f
hyeonji826 <shguswl826@gmail.com>
2025-10-18 19:50:06 +0900

    db: use MariaDB only; remove H2 runtime; disable Flyway; schema.sql as single source

--- modules/infrastructure/persistence/build.gradle.kts
+++ modules/infrastructure/persistence/build.gradle.kts
@@ -10,7 +10,6 @@ dependencies {
     implementation(projects.modules.domain)
     implementation(projects.modules.application)
     implementation(libs.spring.boot.starter.jpa)
-    runtimeOnly(libs.database.h2)
     runtimeOnly(libs.database.mariadb)
 
     testImplementation(libs.spring.boot.starter.test) {
